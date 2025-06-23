2dab24a20a8e2e4d9337400349caeca4ecd3509f
Matheesha Dissanayake <SA22524904@my.sliit.lk>
2025-06-23 08:34:54 +0530

    updated

--- src/app.jsx
+++ src/app.jsx
@@ -15,7 +15,6 @@ const App = () => {
       <Hero />
       <BrowserRouter>
         <ShowcaseSection />
-        {/* <LogoShowcase /> */}
         <FeatureCards />
         <TechStack />
         <ExperienceSection />
--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  }
 });
